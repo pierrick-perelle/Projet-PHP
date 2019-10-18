9ef3f8b1a99dd30163fd025f52c0180519332440
William-Viala <william.viala@etu?umontpellier.fr>
2019-10-18 16:24:43 +0200

    mise à niveau depuis master

--- model/Model.php
+++ model/Model.php
@@ -6,11 +6,14 @@ class Model{
     public static $pdo;
         
     public static function Init(){
-        $hostname = Conf::getHostname();
-	$database_name = Conf::getDatabase();
-	$login = Conf::getLogin();
-	$password = Conf::getPassword();
         try{
+            $login = Conf::getLogin();
+            $database_name = Conf::getDatabase();
+            $password = Conf::getPassword();
+            $hostname = Conf::getHostname();
+            // Connexion à la base de données            
+            // Le dernier argument sert à ce que toutes les chaines de caractères 
+            // en entrée et sortie de MySql soit dans le codage UTF-8
             self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,
             array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             
