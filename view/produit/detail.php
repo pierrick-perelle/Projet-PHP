2575b9729f4fd6d36d762db4a66872a6f113065a
thomas rouzes <thomas.rouzes@etu.umontpellier.fr>
2019-10-18 16:00:52 +0200

    view done

---
+++ view/produit/detail.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Detail des produits</title>
+</head>
+<body>
+<?php
+echo  'Produit: '.$v->getIdProduit().' libelle: ' . $v->getLibelle() . ' prix: '.$v->getPrixProduit() . ' stock: '.$v->getStock() . ' description: ' . $v->getDescription() ;
+
+?>
+</body>
+</html>
