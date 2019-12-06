1724b8ef279314b7c73c52fbeb02de64932f8860
thomas rouzes <thomas.rouzes@etu.umontpellier.fr>
2019-12-06 14:29:25 +0100

    liste user s'affiche que quand on est admin

--- model/ModelUtilisateur.php
+++ model/ModelUtilisateur.php
@@ -8,23 +8,20 @@ class ModelUtilisateur extends Model {
     private $mailClient;
     private $mdp;
     private $adresseClient;
-    protected static $object = 'utilisateur';
+    protected static $objet = 'utilisateur';
     protected static $primary='login';
-
     // Getter générique 
     public function get($nom_attribut) {
         if (property_exists($this, $nom_attribut))
             return $this->$nom_attribut;
         return false;
     }
-
     // Setter générique
     public function set($nom_attribut, $valeur) {
         if (property_exists($this, $nom_attribut))
             $this->$nom_attribut = $valeur;
         return false;
     }
-
     // un constructeur
     public function __construct($login = NULL, $nom = NULL, $prenom = NULL, $mailClient = NULL, $mdp = NULL, $adresseClient=NULL) {
         if (!is_null($login) && !is_null($nom) && !is_null($prenom)) {
@@ -36,7 +33,6 @@ class ModelUtilisateur extends Model {
             $this->adresseClient=$adresseClient;
         }
     }
-
     public static function checkPassword($login,$mot_de_passe_chiffre){
           $sql = "SELECT * FROM utilisateur WHERE login=:login AND mdp=:mdp";
           // Préparation de la requête
@@ -55,5 +51,16 @@ class ModelUtilisateur extends Model {
           } else {
             return true;
           }
+    }
+    public static function checkAdmin($login){
+        $table_name = static::$objet;
+        $sql = "SELECT admin from ".$table_name." WHERE login='".$login."'";
+        $rep = Model::$pdo->query($sql);
+        $admin = $rep->fetchAll();
+        if ($admin[0]['admin']==0){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
